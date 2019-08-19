b606e91b6a59fd36ddab1b202fec626dc8186d1d
Vinicius Fernandes <vnferna@gmail.com>
2019-08-19 14:09:03 -0300

    ajusts na class de parametros

--- Class/parametros.class.php
+++ Class/parametros.class.php
@@ -1,34 +1,41 @@
 <?php
 	class Parametros {
+		private $db;
 
-		public function buscaValor($para_nome, $db){
-			
+		function __construct($db){
+			$this->db = $db;
 		}
 
-		public function gravaValor($id, $dados, $db){
+		public function buscaValor($para_nome){
+			$sql = "SELECT para_valor FROM parametros WHERE para_nome = '{$para_nome}'";
+			$para_valor = $this->db->retornaUmCampoSql($sql, "para_valor");
+			return $para_valor;
+		}
+
+		public function gravaValor($id, $dados){
 			if(intval($id) > 0){
 				$campoID = "idparametros";
 			}else{
 				$campoID = "para_nome";
 			}
 			//
-			$db->setTabela("parametros", $campoID);
-		    $db->gravarInserir($dados);
+			$this->db->setTabela("parametros", $campoID);
+		    $this->db->gravarInserir($dados);
 		}
 
-		public function retornaDados($idparametros, $db){
+		public function retornaDados($idparametros){
 			$sql = "SELECT * FROM parametros WHERE idparametros = {$idparametros}";
-			$res = $db->retornaUmReg($sql);
+			$res = $this->db->retornaUmReg($sql);
 			return $res;
 		}
 
-		public function tabelaParametros($filtro, $db, $util){
+		public function tabelaParametros($filtro, $util){
 			$sql = "SELECT * FROM parametros";
 			if(!empty($filtro)){
 				$sql .= " WHERE para_nome LIKE " . $util->sgr("%" . $filtro ."%");
 			}
 			$sql .= " ORDER BY para_nome ASC";
-			$res = $db->consultar($sql);
+			$res = $this->db->consultar($sql);
 			$linhaColorida = false;
 		    $tabela .= "<table class='table' style='margin-top: 3px'>";
 			foreach ($res as $reg) {
@@ -44,7 +51,7 @@
 				//
 				$tabela .= "<tr {$class} data-toggle='modal' data-target='#editaParametros' onclick='editaParametros({$reg["idparametros"]})' style='cursor:pointer'>";
 					$tabela .= "<td width='6%'>{$reg["idparametros"]}</td>";
-					$tabela .= "<td>{$reg["para_nome"]}<img src='../icones/duvida.png' width='10px' data-toggle='tooltip' title='{$reg["para_obs"]}' style='cursor: '></td>";
+					$tabela .= "<td><img src='../icones/duvida.png' width='12px' id='help_{$reg["idparametros"]}' data-toggle='tooltip' data-placement='top' title='{$reg["para_obs"]}' style='cursor: help; float: left;'>{$reg["para_nome"]}</td>";
 					$tabela .= "<td align='right' width='20%' id='para_valor_{$reg["idparametros"]}'>{$reg["para_valor"]}</td>";
 				$tabela .- "</tr>";
 			}
